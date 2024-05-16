d2c4fa944b26d99d1752386de5596e396b21f241
leon3108 <maxime.noel@epitech.eu>
2024-05-16 15:10:51 +0200

    add placeholder image if image not found in recipe

--- src/app/Recipes/Recipe.tsx
+++ src/app/Recipes/Recipe.tsx
@@ -16,6 +16,7 @@ import {
 import { Eye, EyeOff } from 'lucide-react';
 import Image from 'next/image';
 import { useState } from 'react';
+import placeholderImage from '../../../public/placeholder.jpg';
 import IngredientsWithQuantity from '../Ingredients/IngredientsWithQuantity';
 import Steps from '../Steps/Steps';
 import { Tags } from '../Tags/Tags';
@@ -24,24 +25,25 @@ import { RecipeType } from './RecipeType';
 export const Recipe = ({ recipe }: { recipe: RecipeType }) => {
   const [showSteps, setShowSteps] = useState<Boolean>(false);
   const [showIngredients, setShowIngredients] = useState<boolean>(false);
+  const [imgSrc, setImgSrc] = useState(recipe.imageURL);
 
   return (
     <Card className='mt-5 flex flex-col p-3 md:w-[600px]'>
       <CardHeader>
         <CardTitle>{recipe.name}</CardTitle>
         <CardDescription className='text-wrap p-3'>
-          {' '}
           {recipe.description}
         </CardDescription>
       </CardHeader>
       <Tags tags={recipe.tags} />
       <CardContent className='flex flex-row space-x-2'>
         <Image
-          className={`hidden h-52 w-2/3 flex-col justify-evenly bg-cover sm:flex`}
-          src={recipe.imageURL}
+          className={`hidden h-52 w-2/3 flex-col justify-evenly rounded-sm bg-cover sm:flex`}
+          src={imgSrc}
           alt={recipe.name}
           width={500}
           height={300}
+          onError={() => setImgSrc(placeholderImage.src)}
         />
         <div className='w-full rounded-md bg-fire-bush-200 p-3'>
           <div className='flex flex-col items-start rounded-sm bg-fire-bush-100 p-3'>
