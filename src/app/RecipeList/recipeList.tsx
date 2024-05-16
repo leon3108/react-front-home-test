1a4eb2ccec2ffe0d6738ba54d651b60c910094ef
leon3108 <maxime.noel@epitech.eu>
2024-05-16 15:38:47 +0200

    add: image if no recipe suits the filter

--- src/app/RecipeList/recipeList.tsx
+++ src/app/RecipeList/recipeList.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import NoRecipe from '@/public/undraw_No_data_re_kwbl.png';
 import Recipe from '@/recipes/Recipe';
 import { allRecipes } from '@/recipes/RecipeData';
+import Image from 'next/image';
 import { useContext } from 'react';
 import { FilterContext } from '../filterContextProvider';
 
@@ -20,9 +22,19 @@ export default function RecipeList() {
 
   return (
     <section className='flex flex-grow flex-col items-center p-3 lg:ml-10 xl:justify-center '>
-      {filteredRecipes.map((obj) => (
-        <Recipe recipe={obj} key={obj.name} />
-      ))}
+      {filteredRecipes.length === 0 ? (
+        <div className='flex h-full w-full flex-col items-center justify-center'>
+          <p>Dommage pas de recette</p>
+          <Image
+            src={NoRecipe}
+            alt='no recipe found'
+            width={300}
+            height={300}
+          />
+        </div>
+      ) : (
+        filteredRecipes.map((obj) => <Recipe recipe={obj} key={obj.name} />)
+      )}
     </section>
   );
 }
